0b61a287c51d312b4bdf816c2abf00d8e8f6344c
Vedant Kumar Pankaj <82309525+sniper-panda@users.noreply.github.com>
2026-04-24 21:27:40 +0530

    Add: python-dict-values - submission-0

---
+++ Python For Beginners/python-dict-values/submission-0.py
@@ -0,0 +1,12 @@
+from typing import Dict, List
+
+def get_dict_values(age_dict: Dict[str, int]) -> List[int]:
+    values=[]
+    for value in age_dict.values():
+        values.append(value)
+
+    return values
+
+# do not modify below this line
+print(get_dict_values({"Alice": 25, "Bob": 30, "Charlie": 35}))
+print(get_dict_values({"Alice": 25, "Bob": 30, "Charlie": 35, "David": 40}))
